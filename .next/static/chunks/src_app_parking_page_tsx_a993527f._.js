7ad23971d908d3340757a084d0fe405c484bf252
kumaramit34380 <mandalamitkumar1999@gmail.com>
2025-07-29 18:27:27 +0530

    Add .next/static/chunks/src_app_parking_page_tsx_a993527f._.js 1753793845446

---
+++ .next/static/chunks/src_app_parking_page_tsx_a993527f._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_f71d9bf3._.js",
+  "static/chunks/node_modules_6e89e354._.js"
+],
+    source: "dynamic"
+});
